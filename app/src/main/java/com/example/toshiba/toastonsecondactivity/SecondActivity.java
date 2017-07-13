8301797823043452f38c0d127d5f8ce1e3309401
Nabeel Yaseen <nabeel.yaseen10@gmail.com>
2017-07-13 17:57:59 +0500

    Initial Commit

---
+++ app/src/main/java/com/example/toshiba/toastonsecondactivity/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.example.toshiba.toastonsecondactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Toast.makeText(getApplicationContext(), "I said Don't Press", Toast.LENGTH_SHORT).show();
+    }
+}
